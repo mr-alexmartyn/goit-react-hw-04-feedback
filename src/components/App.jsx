2c4a455b797e4fa164e29014db5b7b58d4428df5
mr-alexmartyn <111350218+mr-alexmartyn@users.noreply.github.com>
2023-04-23 12:56:56 +0300

    use hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,45 @@
-import React, { Component } from 'react';
-import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
+import React, { useState } from 'react';
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 import { Section } from 'components/Section/Section';
 import { FeedbackContainer } from 'components/FeedStyled.styled';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const totalStats = () => this.good + this.neutral + this.bad;
+
+  const handleGood = () => {
+    setGood(prev => prev + 1);
+  };
+  const handleNeutral = () => {
+    setNeutral(prev => prev + 1);
+  };
+  const handleBad = () => {
+    setBad(prev => prev + 1);
   };
 
-  totalStats = () => this.state.good + this.state.neutral + this.state.bad;
+  return (
+    <FeedbackContainer>
+      <Section title="Please leave FeedBack">
+        <FeedbackOptions
+          handleGood={handleGood}
+          handleNeutral={handleNeutral}
+          handleBad={handleBad}
+        />
+      </Section>
+      <Section title="Statistics">
+        {totalStats() > 0 ? (
+          <Statistics good={good} neutral={neutral} bad={bad} />
+        ) : (
+          <Notification />
+        )}
+      </Section>
+    </FeedbackContainer>
+  );
+};
 
-  handleGood = () => {
-    this.setState(prevState => {
-      return {
-        good: prevState.good + 1,
-      };
-    });
-  };
-  handleNeutral = () => {
-    this.setState(prevState => {
-      return {
-        neutral: prevState.neutral + 1,
-      };
-    });
-  };
-  handleBad = () => {
-    this.setState(prevState => {
-      return {
-        bad: prevState.bad + 1,
-      };
-    });
-  };
-
-  render() {
-    return (
-      <FeedbackContainer>
-        <Section title="Please leave FeedBack">
-          <FeedbackOptions
-            handleGood={this.handleGood}
-            handleNeutral={this.handleNeutral}
-            handleBad={this.handleBad}
-          />
-        </Section>
-        <Section title="Statistics">
-          {this.totalStats() > 0 ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-            />
-          ) : (
-            <Notification />
-          )}
-        </Section>
-      </FeedbackContainer>
-    );
-  }
-}
 export default App;
--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FeedbackList, FeedbackButton } from 'components/FeedStyled.styled';
 
-class FeedbackOptions extends Component {
-  render() {
-    return (
-      <FeedbackList>
-        <li>
-          <FeedbackButton type="button" onClick={this.props.handleGood}>
-            Good
-          </FeedbackButton>
-        </li>
-        <li>
-          <FeedbackButton type="button" onClick={this.props.handleNeutral}>
-            Neutral
-          </FeedbackButton>
-        </li>
-        <li>
-          <FeedbackButton type="button" onClick={this.props.handleBad}>
-            Bad
-          </FeedbackButton>
-        </li>
-      </FeedbackList>
-    );
-  }
+function FeedbackOptions() {
+  return (
+    <FeedbackList>
+      <li>
+        <FeedbackButton type="button" onClick={this.props.handleGood}>
+          Good
+        </FeedbackButton>
+      </li>
+      <li>
+        <FeedbackButton type="button" onClick={this.props.handleNeutral}>
+          Neutral
+        </FeedbackButton>
+      </li>
+      <li>
+        <FeedbackButton type="button" onClick={this.props.handleBad}>
+          Bad
+        </FeedbackButton>
+      </li>
+    </FeedbackList>
+  );
 }
 
 export default FeedbackOptions;
